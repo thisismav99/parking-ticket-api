bf8cd3da9f86047a5b5495de60f04cc0648562e3
Mave Rick Paz <mave.paz@proactis.com>
2025-11-19 22:11:17 +0800

    Add swagger

--- ParkingTicketingAPI/Program.cs
+++ ParkingTicketingAPI/Program.cs
@@ -12,15 +12,17 @@ builder.Services.RegisterApplication();
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
